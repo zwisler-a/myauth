721ff1b9b68400f1009b020f5dede6b56ae2229f
zwisler <alex-z-@web.de>
2019-08-20 20:42:29 +0200

    fix: auth service script host

--- ui/src/views/Users/UserProperties.vue
+++ ui/src/views/Users/UserProperties.vue
@@ -1,12 +1,38 @@
 <template>
-  <div></div>
+  <div>
+    <select v-model="selectedRealm">
+      <option :value="realm.id" v-for="realm in realms" :key="realm.id">{{realm.name}}</option>
+    </select>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { Notification } from '../../services/notif.service';
+import { Component, Vue } from "vue-property-decorator";
+import { Notification } from "../../services/notif.service";
+import { PropService } from "../../services/prop.service";
+import { RealmService } from "../../services/realm.service";
+import { Realm } from "../../model/realm.interface";
 
 @Component
 export default class UserProperties extends Vue {
+  private realms: Realm[] = [];
+  private _selectedRealm: string = "";
+  private propService: PropService = PropService.getInstance();
+  private realmService: RealmService = RealmService.getInstance();
+
+  public async mounted() {
+    this.realms = await this.realmService.getRealms();
+  }
+
+  get selectedRealm() {
+    return this._selectedRealm;
+  }
+  set selectedRealm(value: string) {
+    this._selectedRealm = value;
+  }
+
+  private async loadProperties(realmId: string, userId: string) {
+    await this.propService.get(realmId, userId);
+  }
 }
 </script>
